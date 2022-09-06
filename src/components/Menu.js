056459cce697db0826fa9b225a4ddf533de4ef2e
Charles <charles.dalisay@globe.com.ph>
2022-09-06 19:50:34 +0800

    create hook for chaning page

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,6 +1,10 @@
 import { Link, Box, Text } from "@chakra-ui/react";
 
+import useMenu from "../hooks/useMenu";
+
 const Home = (props) => {
+  const [setPage] = useMenu();
+
   return (
     <Box p={3} m={3} mb={0} pt={0}>
         <Text className="menu" >
@@ -8,23 +12,23 @@ const Home = (props) => {
         </Text>
         <Text>
             [0]&nbsp;
-            <Text as={Link} onClick={(event) => props.handleKeyPress(event, '0')}>Bio</Text>
+            <Text as={Link} onClick={(event) => setPage('0')}>Bio</Text>
         </Text>
         <Text>
             [1]&nbsp;
-            <Text as={Link} onClick={(event) => props.handleKeyPress(event, '1')}>Experience</Text>
+            <Text as={Link} onClick={(event) => setPage('1')}>Experience</Text>
         </Text>
         <Text>
             [2]&nbsp;
-            <Text as={Link} onClick={(event) => props.handleKeyPress(event, '2')}>Projects</Text>
+            <Text as={Link} onClick={(event) => setPage('2')}>Projects</Text>
         </Text>
         <Text>
             [3]&nbsp;
-            <Text as={Link} onClick={(event) => props.handleKeyPress(event, '3')}>Contact</Text>
+            <Text as={Link} onClick={(event) => setPage('3')}>Contact</Text>
         </Text>
         <Text>
             [4]&nbsp;
-            <Text as={Link} onClick={(event) => props.handleKeyPress(event, '4')}>I <Text as="b" color="red">♥</Text></Text>
+            <Text as={Link} onClick={(event) => setPage('4')}>I <Text as="b" color="red">♥</Text></Text>
         </Text>
         <Text>[5] Dark | Light</Text>
     </Box>
