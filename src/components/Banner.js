34faf83cb9c94befcd4d42ac62640bd9b616e305
Charles <charles.dalisay@globe.com.ph>
2022-09-06 22:37:05 +0800

    change banner art

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,13 +2,11 @@ import { Box, Text } from "@chakra-ui/react";
 
 const Banner = () => {
   return (
-    <Box p={3} m={3} pt={0} pb={0}  fontWeight="lighter" >
-        <Text as="pre">:'######:::'######::'########::</Text>
-        <Text as="pre">'##... ##:'##... ##: ##.... ##:</Text>
-        <Text as="pre"> ##::::::: ##::::::: ##:::: ##:</Text>
-        <Text as="pre"> ##::: ##: ##::: ##: ##:::: ##:</Text>
-        <Text as="pre">. ######::. ######:: ########::</Text>
-        <Text as="pre">:......::::......:::........:::</Text>
+    <Box p={3} m={3} pt={0} pb={0}  fontWeight="bold" >
+        <Text as="pre">         /\_/\</Text>
+        <Text as="pre">    ____/ o o \</Text>
+        <Text as="pre">  /~____  =ø= /</Text>
+        <Text as="pre"> (______)__m_m)</Text>
     </Box>
   );
 };
