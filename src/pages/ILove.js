44990e6309ca9180e30cceef5a34d565bc4858c5
Charles <charles.dalisay@globe.com.ph>
2022-09-12 11:26:45 +0800

    put emphasizeto heart

--- src/pages/ILove.js
+++ src/pages/ILove.js
@@ -7,7 +7,7 @@ const ILove = () => {
       <Header text={"I ❤"} component="span"/>
       <Text as="b">Maybe we can be friend!</Text>
       <br />
-      <Text>I ❤ Cat, Eating, Music, Photography, Problems :), Learn new things</Text>
+      <Text>I <Text as="b" color="red">❤</Text> Cat's, Eating, Music, Photography, Problems :), Learn new things</Text>
       <br />
       <Text >@ Hobbies</Text>
       <br />
@@ -15,7 +15,7 @@ const ILove = () => {
           <GridItem colSpan={12}>
             <Text>Gaming ~ <Text as="span">I'm not good at it, but I play a lot of chess.</Text></Text>
             <Text>Reading ~ <Text as="span">I've read a lot of fantasy and sci-fi novels.</Text></Text>
-            <Text>Cycling ~ <Text as="span">Long / Short / Trails ride G.</Text></Text>
+            <Text>Cycling ~ <Text as="span">Long, short, trails ride let's go.</Text></Text>
           </GridItem>
       </Grid>
     </Box>
