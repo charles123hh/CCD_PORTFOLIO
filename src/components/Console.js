e76f3a429a6117d4f98cb3ef8f0541a5c286eb1a
Charles <charles.dalisay@globe.com.ph>
2022-09-12 12:10:41 +0800

    remove experience page / add cv file / refactor menu indexing

--- src/components/Console.js
+++ src/components/Console.js
@@ -5,21 +5,26 @@ import ConsoleInput from "./ConsoleInput";
 import useMenu from "../hooks/useMenu";
 import useMode from "../hooks/useMode";
 
+import CV from "../resources/CV.pdf";
+
 const Comsole = () => {
   const [activeMenu, setMenu,  getCurrentLocation] = useMenu();
   const [activeMode, setMode] = useMode();
 
   const handleKeyEvent = (event) => {
-    if (event.keyCode === 13 && event.target.value === '5') {
+    if (event.keyCode === 13 && event.target.value === '7') {
         if(activeMode("light")) {
            setMode("light");
         } else {
           setMode("dark");
         }
         event.target.value = "";
-    } else if (event.keyCode === 13 && event.target.value !== 5) {
-        setMenu(event.target.value);
+    } else if (event.keyCode === 13 && event.target.value === '6') {
+        window.open(CV, "_blank")
         event.target.value = "";
+    } else if (event.keyCode === 13 && event.target.value !== '6' && event.target.value !== '7') {
+      setMenu(event.target.value);
+      event.target.value = "";
     }
   }
 
@@ -36,7 +41,7 @@ const Comsole = () => {
   let console;
 
   if(activeMenu('')){
-    console = <ConsoleInput text="Select an option to continue 0 ~ 5" location={getCurrentLocation()} handleKeyEvent = {handleKeyEvent} placeholder={"ex. 0"}/>;
+    console = <ConsoleInput text="Select an option to continue 1 ~ 6" location={getCurrentLocation()} handleKeyEvent = {handleKeyEvent} placeholder={"ex. 1"}/>;
   } else {
     console = <ConsoleInput text="Press enter key to continue..." location={getCurrentLocation()} handleKeyEvent = {handleKeyEvent_2} />;
   }
